cdb63a54175c26698079776f17ad8d9299d5f01c
srini akhil <sriniakhil13@gmail.com>
2018-01-21 16:43:53 +0530

    Update bdayresponse.py

--- bdayresponse.py
+++ bdayresponse.py
@@ -1,9 +1,9 @@
-# -*- coding: utf-8 -*-
+
 
 import sys
 import cursor
 import os
-#import maths
+
 from urllib import urlencode
 import requests
 from urlparse import urlparse, parse_qs
@@ -14,7 +14,6 @@ import calendar
 
 #settings
 
-#datetime(year, month, day)
 bday = datetime(2014, 8, 25)
 
 
@@ -33,7 +32,6 @@ message_set = ['Thank you very much', 'Thanks a lot', 'Thank you!']
 #if false, repies to every message. Make it false if you are sure every wish posted on your wall is a birthday message
 use_filter = True
 
-#keywords to respond to. Comment only on posts containing at lease one of these words
 bdaywords = ["happy", "bday", "b\'day", "birthday","hbd", "wish", "returns", u"cumpleaños".encode("utf-8"),"anniversaire","compleanno","Geburtstag"]
 
 
@@ -64,12 +62,12 @@ def get_posts(url, wishes=None):
             
             content = req.json()
             
-            #keep only relevant fields from post data
+            
             feed = []
             for post in content['data']:
                 feed.append({'id': post['id'],'from': post['from']['name'],'message': post.get('message', ''),'type': post['type']})
 
-            #keep only posts relevant to birthday. Make sure you reply your friends who post happy birthday pictures on your timeline or posts in local language
+           
             for post in feed:
                 if post['type']=='status' and is_birthday(post['message'], use_filter) :
                     wishes.append(post)
@@ -109,20 +107,20 @@ def is_birthday (message, filter):
 
 if __name__ == '__main__':
     
-    #get bithday wishes
+    
     base_url = 'https://graph.facebook.com/v2.1/me/feed'
     params = {'since': utc_bday, 'access_token': access_token}
     url = '%s?%s' % (base_url, urlencode(params))
     posts = get_posts(url)
     
-    #confirm before posting
+    
     usersignal = confirm('Found %s birthday wishes. Ready to thank them?' % (len(posts)))
     
     #post if user said yes
     if usersignal is True:
         for post in posts:
 
-            #thank the user
+            
             if comment:
                 reply = choice(message_set)
                 print 'Replying %s to %s' % (reply, wish['from'])
